627930dda6f662722bfaead83ea86f06ea71cdb4
jacob berkman <jacob@87k.net>
2015-03-05 10:11:19 -0800

    Initial Commit

---
+++ N87kMechanics/N87kMechanics.h
@@ -0,0 +1,19 @@
+//
+//  N87kMechanics.h
+//  N87kMechanics
+//
+//  Created by jacob berkman on 2015-03-05.
+//  Copyright (c) 2015 jacob berkman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for N87kMechanics.
+FOUNDATION_EXPORT double N87kMechanicsVersionNumber;
+
+//! Project version string for N87kMechanics.
+FOUNDATION_EXPORT const unsigned char N87kMechanicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <N87kMechanics/PublicHeader.h>
+
+
